a8c734c423abf9c180f400c040d8ef6f77d0c3f0
Spectu <Santymartinez913@gmail.com>
2020-11-18 20:40:38 -0500

    Actulizado

--- dist/bot.js
+++ dist/bot.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
 dotenv_1.config();
@@ -8,10 +17,25 @@ const prefix_json_1 = require("./prefix.json");
 client.on("ready", () => {
     console.log(`Bot is ready!`);
 });
-client.on("message", (message) => {
+client.on("message", (message) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b;
     console.log(message.content);
-    if (message.content.startsWith(`${prefix_json_1.prefix}hola`)) {
-        message.reply(`Adios`);
+    if (message.content.startsWith(`${prefix_json_1.prefix}kick`)) {
+        if ((_a = message.member) === null || _a === void 0 ? void 0 : _a.hasPermission(['ADMINISTRATOR', 'KICK_MEMBERS'])) {
+            const member = (_b = message.mentions.members) === null || _b === void 0 ? void 0 : _b.first();
+            if (member) {
+                const bannedmember = yield member.kick();
+                message.channel.send('The user' + bannedmember.user.username + 'has been kicked');
+            }
+        }
+    }
+    else {
+        message.reply('You dont have permission to use this command');
+    }
+}));
+client.on("message", message => {
+    if (message.content.startsWith(`${prefix_json_1.prefix}hello`)) {
+        message.reply('hi!');
     }
 });
 client.login(process.env.TOKEN);
--- src/bot.ts
+++ src/bot.ts
@@ -10,11 +10,24 @@ client.on("ready", () => {
 console.log(`Bot is ready!`)
 })
 
-client.on("message", (message: Message) => {
+client.on("message", async (message: Message) => {
 console.log(message.content)
+if(message.content.startsWith(`${prefix}kick`)){
+    if(message.member?.hasPermission(['ADMINISTRATOR', 'KICK_MEMBERS'])){       
+    const member = message.mentions.members?.first();
+    if(member){
+        const bannedmember = await member.kick();
+        message.channel.send('The user' + bannedmember.user.username + 'has been kicked')
+    }
+  }
+}else{
+    message.reply('You dont have permission to use this command')
+}
+})
 
-if(message.content.startsWith(`${prefix}hola`)){
-message.reply(`Adios`);
+client.on("message", message => {
+    if(message.content.startsWith(`${prefix}hello`)){
+        message.reply('hi!')
     }
 })
 
