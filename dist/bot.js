70c5af57c5b6aff22b9c9729a135b8479260e30c
Spectu <Santymartinez913@gmail.com>
2020-11-23 13:46:21 -0500

    first commit

--- dist/bot.js
+++ dist/bot.js
@@ -5,12 +5,35 @@ dotenv_1.config();
 const discord_js_1 = require("discord.js");
 const client = new discord_js_1.Client();
 const prefix_json_1 = require("./prefix.json");
+const embed = discord_js_1.MessageEmbed;
 client.on("ready", () => {
     console.log(`Bot is ready!`);
 });
 client.on("message", message => {
     if (message.content.startsWith(`${prefix_json_1.prefix}hello`)) {
-        message.channel.send('hi!');
+        message.reply('hi!');
+    }
+});
+client.on("message", message => {
+    if (message.content.startsWith(`${prefix_json_1.prefix}ClearChannel`)) {
+        const { member, mentions } = message;
+        const tag = member === null || member === void 0 ? void 0 : member.id;
+    }
+});
+client.on("message", message => {
+    var _a;
+    if (message.content.startsWith(`${prefix_json_1.prefix}UserName`)) {
+        const usertag = (_a = message.mentions.members) === null || _a === void 0 ? void 0 : _a.first();
+        if (usertag) {
+            var remove = "!!UserTag";
+            var msg = message.content.substring(remove.length);
+            const username = message;
+            const member = message;
+            const tag = `${username}`;
+            msg = tag;
+            console.log(message.mentions.users);
+            message.channel.send(msg);
+        }
     }
 });
 client.login(process.env.TOKEN);
--- src/bot.ts
+++ src/bot.ts
@@ -1,17 +1,42 @@
 import {config} from 'dotenv'
 config();
-import { Client, Message } from 'discord.js';
+import { Client, Message, MessageEmbed } from 'discord.js';
 const client: Client = new Client()
 import { prefix } from './prefix.json';
-
+const embed = MessageEmbed;
 client.on("ready", () => {
 console.log(`Bot is ready!`)
 });
 
 client.on("message", message => {
     if(message.content.startsWith(`${prefix}hello`)){
-        message.channel.send('hi!')
+        message.reply('hi!')
     } 
 });
+
+client.on("message", message => {
+    if(message.content.startsWith(`${prefix}ClearChannel`)){
+    const {member, mentions} = message
+    const tag = member?.id
+    }
+})
+
+client.on("message", message => {
+    if(message.content.startsWith(`${prefix}UserName`)){
+    const usertag = message.mentions.members?.first();
+    if(usertag){
+        var remove = "!!UserTag"
+        var msg = message.content.substring(remove.length);
+        const username = message;
+        const member = message;
+        const tag = `${username}`;
+        msg = tag
+        console.log(message.mentions.users);
+        message.channel.send(msg);
+        
+        }
+    }
+})
+
 
 client.login(process.env.TOKEN);
